919e052d287ec56769b2fb14a76743364e8d7706
Kamil Mosciszko <kamil.mazurek319@gmail.com>
2024-10-23 11:25:13 +0200

    Feat 1 (#4)

--- client/main.go
+++ client/main.go
@@ -17,14 +17,9 @@ type Client struct {
 	AuthClient AuthClient
 }
 
-func (client *Client) listJobs(ctx context.Context, page string, number string) (*ListJobReply, error) {
+func (client *Client) listJobs(ctx context.Context, req *ListJobRequest) (*ListJobReply, error) {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
 	defer cancel()
-
-	req := &ListJobRequest{
-		Page:   page,
-		Number: number,
-	}
 
 	resp, err := client.JobClient.ListJobs(ctx, req)
 	if err != nil {
@@ -143,7 +138,10 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("ReadJob response:", recruit)
-	list, err := client.listJobs(ctx, "1", "100")
+	list, err := client.listJobs(ctx, &ListJobRequest{
+		Page:   "1",
+		Number: "5",
+	})
 	if err != nil {
 		panic(err)
 	}
